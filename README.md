3e66803870f0f74be0312f083a7f5b56ffacc373
crisrib <103081260+crisrib@users.noreply.github.com>
2022-09-04 09:44:53 -0400

    Complete

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # hha-data-cleaning
-HHA 507 - Assignment 2
+HHA 507 - Assignment 2 Instructions
+1. Loaded the data into python.
+
+2. Printed the total count of columns and row.
+
+3. Provided a print out of the column names.
+
+4. Cleaned the column name by removing all special characters and whitespace and replacing all whitespace in column names with an underscore.
+
+5. Cleaned the strings that might exist within each column by firstly getting column types list.
+
+6. Assessed white space or special characters by removing it with a script.
+
+7. Converted the column types to the correct types (e.g., DOB field is date-time and not object).
+
+8. Looked for duplicate rows and remove duplicate rows.
+
+9. Assessed missingness (count of missing values per column) 
+
+10. Created a new column called modality_inperson that  contains a binary value of true or false.
+
+11. Saved the clean data as a new cvs file.
--- cleaning.py
+++ cleaning.py
@@ -26,3 +26,22 @@ list(df)
 # get column types list, want to see if strings are really strings, number are numbers, dates are dates, and boolean are booleans
 df.dtypes
 
+## 6. assess white space or special characters by removing it with a script
+df.columns = df.columns.str.replace('[^A-Za-z0-9]+', '_')
+
+## 7. convert the column types to the correct types by first seeing if date is on weekday or weekend
+df['Week'] = pd.to_datetime(df['Week'])
+df['Week'] = pd['Week'].dt.dayofweek
+
+## 8. look for duplicate rows and remove duplicate rows
+df.drop_duplicates(subset=None, keep='first', inplace=False, ignore_index=False)
+
+## 9. assess missingness (count of missing values per column)
+df.isnull().sum()
+
+## 10. create a new column called modality_inperson that contains a binary value of true or false
+df['modality_inperson'] = np.where(df["Learning_Modality"] == 'In Person', True, False)
+df.dtypes
+
+## 11. save the clean data into a cvs file
+df.to_csv('data/Clean_School_Learning_Modalities.csv')
